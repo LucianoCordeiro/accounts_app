992378468060517ad633498226a61f7e99f4b23e
Luciano Cesar <lucianocordeiro1991@hotmail.com>
2024-06-26 23:20:32 -0300

    create balance request

--- controllers/account_controller.rb
+++ controllers/account_controller.rb
@@ -1,5 +1,15 @@
 class NotFoundError < StandardError; end;
 
+get '/balance' do
+  account = Account.find(params[:account_id]) || raise(NotFoundError)
+
+  status 200
+  json(account.balance)
+rescue NotFoundError => _
+  status 404
+  json(0)
+end
+
 post '/event' do
   body_params = JSON.parse(request.body.read, {symbolize_names: true})
 
